b6ca00a5a74754897ab44e253779c44cffae99a8
Raju03-git <65073232+Raju03-git@users.noreply.github.com>
2021-11-16 12:06:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,8 @@ const io = new Server(server, {
   },
 });
 
-const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Keboli REST API');
-});
-
+let users = {};
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
@@ -31,32 +27,42 @@ io.on("connection", (socket) => {
     console.log('<>Joined<>');
     console.log('');
     console.log('');
+    const user = { username: data.username, room: data.room };
+
+    users[socket.id] = user;
     socket.join(data.room);
     console.log(`User ${data.username} with ID: ${socket.id} joined room: ${data}`);
     const userJoinedMsg = `${data.username} has joined the chat`;
     const chatDet = { room: data.room, author: 'chatBot', message: userJoinedMsg, time: '13:3' }
     console.log('<><>chatDet<><>', chatDet);
     socket.to(data.room).emit("user_joined", chatDet);
+    socket.to(data.room).emit("active_users", users);
     console.log('<><>chatDet<><>', chatDet);
-  }); 
+  });
 
-  socket.on("send_message", (data) => { 
+  socket.on("send_message", (data) => {
     console.log('<><>send_message<><>', data);
     let clientInstance = new message(data);
     clientInstance.save();
     let uniqueMessages = message.find({});
-    console.log('<>uniqueMessages<>',uniqueMessages)
+    //console.log('<>uniqueMessages<>', uniqueMessages)
     socket.to(data.room).emit("receive_message", data);
   });
 
-  socket.on("disconnect", (data) => {
-    console.log('<><>data<><>', data);
+  socket.on("disconnect", () => {
+    console.log('<><>data<><>', users);
     console.log("User Disconnected", socket.id);
-    const userJoinedMsg = `${data.username} has been left from the chat`;
-    const chatDet = { room: data.room, author: 'chatBot', message: userJoinedMsg, time: '13:3' }
-    console.log('<><>chatDet<><>', chatDet);
-    socket.to(data.room).emit("user_left", chatDet);
-    console.log('<><>chatDet<><>', chatDet);
+
+
+    let user = users[socket.id];
+    console.log('<><>user<><>',user);
+    if (user) {
+      const userJoinedMsg = `${user.username} has been left from the chat`;
+      const chatDet = { room: user.room, author: 'chatBot', message: userJoinedMsg, time: '13:3' }
+      console.log('<><>chatDet<><>', chatDet);
+      socket.to(data.room).emit("user_left", chatDet);
+      console.log('<><>chatDet<><>', chatDet);
+    }
   });
 });
 
